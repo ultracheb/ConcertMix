7be132487f5b121d70e89936c7f2be826f99a378
ultracheb <needforgrib@mail.ru>
2014-10-12 19:28:48 +0300

    Event list

--- src/com/example/ConcertMix/MainActivity.java
+++ src/com/example/ConcertMix/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends Activity {
                 manager.createXML(query);
                 events = manager.parseXML("input.xml");
                 paintList();
+                events.clear();
                 return true;
             }
         };
@@ -54,6 +55,7 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
         return true;
     }
 
